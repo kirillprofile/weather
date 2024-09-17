9b75005a464d6733ead520e2ba7acd63d6963ad5
kirill <k.yatskovskiy.work@gmail.com>
2024-09-18 02:03:05 +0300

    Added layout for weather display component: - Displayed city name - Shown maximum and minimum temperatures - Positioned and styled weather icon

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,23 +1,43 @@
-<script setup>
-import pageHeader from './components/pageHeader.vue';
-import nowForecastDisplay from './components/nowForecastDisplay.vue';
-import { ref } from 'vue';
+  <script setup>
+  import pageHeader from './components/pageHeader.vue';
+  import nowForecastDisplay from './components/nowForecastDisplay.vue';
+  import { ref } from 'vue';
 
-const weatherData = ref(null);
+  const weatherData = ref(null);
+  const showNowForecastDisplay = ref(false);
 
-const forwardingCityOperator = (city) => {
-   // api implementation
-   alert(city.city);
-}
+  const forwardingCityOperator = (city) => {
+    // api implementation
+    weatherData.value = {
+        'now': {
+          'city': city.city,
+          'temperature_max': 12,
+          'temperature_min': -3
+        },
+        'now_details': {
+          'sunrise': '8:18',
+          'sunset': '18:40',
+          'chance_of_rain': 21,
+          'pressure': 1023
+        },
+        'todays_forecast': {
+          '6:00': '',
+          '9:00': '',
+          '12:00': '',
+          '15:00': '',
+        }
+    }
+  }
 
-</script>
+  </script>
 
 
-<template>
-  <pageHeader @forwardingCityInput="forwardingCityOperator"/>
-  <nowForecastDisplay/>
-</template>
+  <template>
+    -><div v-if="showNowForecastDisplay">{{ weatherData.now }}</div>
+    <pageHeader @forwardingCityInput="forwardingCityOperator"/>
+    <nowForecastDisplay v-if="weatherData" :weatherNow="weatherData.now"/>
+  </template>
 
-<style>
+  <style>
 
-</style>
+  </style>
